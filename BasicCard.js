36bdc72e2bce2e3a45a24cabadd313630552e7c3
Sarah Diaz-Rivera <sarahjeanlinnebur@yahoo.com>
2017-07-28 22:53:15 -0600

    failing for loop

--- BasicCard.js
+++ BasicCard.js
@@ -8,19 +8,5 @@ var BasicCard = function(frontArg, backArg){
 	this.back = backArg;
 };
 
-var readFile = function(cb) {
-	fs.readFile("basicPresidents.txt", "utf8", function(err, data){
-		if (err) throw err;
-		var dataTxt = data.split("; ");
-		for(i = 0; i < dataTxt.length; i++){
-			var splitTxt= dataTxt[i].split(", ");
-			console.log(splitTxt);
-		};
-
-	});
-};
-
-readFile();
-
 module.exports = BasicCard;
 
---
+++ main.js
@@ -0,0 +1,76 @@
+var fs = require('fs');
+var inquirer = require("inquirer");
+var BasicCard = require('./basicCard.js');
+
+//prompt basic or cloze
+//if basic or cloze, prompt question (front of card) associated with card type
+	//create a for loop that runs through the question array
+	//ask the back question, prompt a response
+	//if response === back, console.log "correct!"
+
+var basicCards = []; //array to hold all the cards objects
+var getCardsFromFile = function() {
+	debugger;
+	var cardData = fs.readFileSync("basicPresidents.txt", "utf8"); //grabbing the info from the text file
+	var cardLines = cardData.split("\n"); //split the items in text file on a new line
+	for(i = 0; i < cardLines.length; i++){
+		var questionAnswerPair = cardLines[i].split(","); 
+		var answer = questionAnswerPair[0];
+		var question = questionAnswerPair[1];
+		var newCard = new BasicCard(question, answer);
+		basicCards.push(newCard);
+	}
+	//return basicCards;
+};
+
+var cardType = function(){
+	inquirer.prompt([{
+		type: "input",
+		message: "Cloze or Basic?",
+		name: "card"
+	}]).then(function(inquirerResponse){
+		switch (inquirerResponse.card){
+			case "basic": 
+			debugger;
+			getCardsFromFile();
+			loopCards();
+			break;
+
+			case "cloze":
+			console.log("chose cloze");
+			break;
+		}
+	});
+}; 	
+
+var loopCards = function(){
+	debugger;
+	for (i = 0; i < basicCards.length; i++){
+		inquirer.prompt([{
+			type: "input",
+			message: basicCards[i].back,
+			name: "back"
+		}]).then(function(inquirerResponse){
+			if (inquirerResponse.back === basicCards[i].back){
+				console.log("correct!");
+			}
+			else {
+				console.log("wrong answer :(");
+			};
+
+			// switch (inquirerResponse.loopCards){
+			// 	case basicCards[i].back:
+			// 	console.log("correct!");
+			// 	break;
+
+			// 	case !basicCards[i].front:
+			// 	console.log("sorry, wrong answer!");
+			// 	break;
+		});
+	};
+};
+
+cardType();
+
+
+//console.log(basicCardsFromFile);
