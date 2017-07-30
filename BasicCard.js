a3d3d6f21203099c4f9dd543c4c4660ab0d0bc8c
Sarah Diaz-Rivera <sarahjeanlinnebur@yahoo.com>
2017-07-30 15:08:40 -0600

    getting questions to ask properly for basic cards

--- BasicCard.js
+++ BasicCard.js
@@ -7,22 +7,6 @@ var BasicCard = function(frontArg, backArg){
 	debugger;
 	this.front = frontArg;
 	this.back = backArg;
-	this.print = function(){
-	debugger;
-		inquirer.prompt([{
-			type: "input",
-			message: this.front,
-			name: "back"
-		}]).then(function(inquirerResponse){
-			if (inquirerResponse.back === this.back){
-				console.log("Correct!");
-			}
-			else {
-				console.log("Sorry, wrong answer.");
-			};
-		});
-	};
-	this.print();
 };
 
 module.exports = BasicCard;
--- main.js
+++ main.js
@@ -19,7 +19,7 @@ var cardType = function(){
 			case "basic": 
 			debugger;
 			getCardsFromFile();
-			loopCards();
+			askQuestion();
 			break;
 
 			case "cloze":
@@ -29,30 +29,53 @@ var cardType = function(){
 	});
 };
 
-//var basicCards = []; //array to hold all the cards objects
+var basicCards = []; //array to hold all the cards objects
 var getCardsFromFile = function() {
 	debugger;
 	var cardData = fs.readFileSync("basicPresidents.txt", "utf8"); //grabbing the info from the text file
 	var cardLines = cardData.split("\n"); //split the items in text file on a new line
 	for(i = 0; i < cardLines.length; i++){
 		var questionAnswerPair = cardLines[i].split(","); 
-		var answer = questionAnswerPair[0];
+		var answer = questionAnswerPair[0].toUpperCase();
 		var question = questionAnswerPair[1];
 		var newCard = new BasicCard(question, answer);
-		//basicCards.push(newCard);
+		basicCards.push(newCard);
 	}
 	//return basicCards;
 }; 	
+
+var index = 0;
+function askQuestion(){
+	debugger;
+	if (index < basicCards.length){
+		inquirer.prompt ([{
+			name: "front",
+			message: basicCards[index].front,
+		}]).then(function(inquirerResponse){
+			var response = inquirerResponse.front.toUpperCase();
+			if (response === basicCards[index].back){
+				console.log("correct!");
+				index++;
+				askQuestion();
+			}
+			else {
+				console.log("incorrect!");
+				index++;
+				askQuestion();
+			};
+		});
+	};
+};
 
 // var loopCards = function(){
 // 	debugger;
 // 	for (i = 0; i < basicCards.length; i++){
 // 		inquirer.prompt([{
 // 			type: "input",
-// 			message: basicCards[i].back,
+// 			message: basicCards[i].front,
 // 			name: "back"
 // 		}]).then(function(inquirerResponse){
-// 			if (inquirerResponse.back === basicCards[i].back){
+// 			if (inquirerResponse.back === basicCards[i].front){
 // 				console.log("correct!");
 // 			}
 // 			else {
